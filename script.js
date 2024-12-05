a6bb4ca056b58fea3839bb014d09d74e85171827
Lubaba Basri <23pwcse2255@uetpeshawar.edu.pk>
2024-12-05 16:14:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function calculateAge() {
+    const dob = document.getElementById("dob").value;
+    const birthDate = new Date(dob);
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+
+    document.getElementById("result").textContent = `Your age is ${age} years old.`;
+}
